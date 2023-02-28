711b13cc02f4cc648df2f8d66306a17f7eadbd05
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-02-28 12:03:52 +0900

    [Feat] : Fetch Dog Image asynchronously ( 1. URLSession.share.dataTask )

---
+++ Wanted_Pre_Onboarding_mission_SKH/DogAPI.swift
@@ -0,0 +1,13 @@
+//
+//  DogAPI.swift
+//  Wanted_Pre_Onboarding_mission_SKH
+//
+//  Created by james seo on 2023/02/28.
+//
+
+import Foundation
+
+struct DogAPI: Decodable {
+    let message: String
+    let status: String
+}
--- Wanted_Pre_Onboarding_mission_SKH/ViewController.swift
+++ Wanted_Pre_Onboarding_mission_SKH/ViewController.swift
@@ -8,16 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    /// dog-api : https://dog.ceo/dog-api/
+    private let urlString: String = "https://dog.ceo/api/breeds/image/random"
+    private let decoder = JSONDecoder()
+    
     @IBOutlet var imageViews: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func didTapLoadButton(_ sender: UIButton) {
-        print(sender.tag, "th button clicked!")
+        switch sender.tag {
+        case self.imageViews.count:
+            // 1. URLSession.shared.dataTask
+            for index in 0..<sender.tag {
+                doubleURLSessionDataTask(index: index)
+            }
+        default:
+            // 1. URLSession.shared.dataTask
+            doubleURLSessionDataTask(index: sender.tag)
+        }
     }
-    
 }
 
+private extension ViewController {
+    func doubleURLSessionDataTask(index: Int) {
+        // 1. image placeholder
+        self.imageViews[index].image = UIImage(systemName: "photo")
+        // 2. fetch data
+        URLSession.shared.dataTask(with: URL(string: urlString)!) { [weak self] data, _, _ in
+            guard let data = data else { return }
+            
+            let dogData = try? self?.decoder.decode(DogAPI.self, from: data)
+            
+            guard dogData?.status == Optional("success") else { return }
+            
+            if let url = dogData?.message {
+                URLSession.shared.dataTask(with: URL(string: url)!) { [weak self] data, _, _ in
+                    guard let data = data,
+                          let image = UIImage(data: data)
+                    else { return }
+                    
+                    DispatchQueue.main.async {
+                        self?.imageViews[index].image = image
+                    }
+                }
+                .resume()
+            }
+        }
+        .resume()
+    }
+}
